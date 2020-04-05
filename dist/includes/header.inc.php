4714a58fde73383fb68918b1fc93d0df8435668c
David Zakrzewski <david.zakrzewski@epitech.eu>
2020-04-05 22:50:04 +0200

    changed nav

--- dist/includes/header.inc.php
+++ dist/includes/header.inc.php
@@ -2,10 +2,10 @@
 	<div class="w-100 px-5 py-4 navbar d-flex flex-row align-items-center">
 		<div id="logo" ><a href="/"><img src="/assets/logo.svg" alt="logo" /></a></div>
 		<div class="nav-items d-flex flex-row align-items-center">
-			<div class="scale-it"><a href="#">About Us</a></div>
-			<div class="ml-4 scale-it"><a href="#">Find a consultant</a></div>
-			<div class="ml-4 scale-it"><a href="#">Simulation</a></div>
+		<div class="scale-it"><a href="/analytics/index.php">Check your compliance</a></div>
 			<div class="ml-4 scale-it"><a href="/contact.php">Contact</a></div>
+			<div class="ml-4 scale-it"><a href="#">About Us</a></div>
+			<div class="ml-4 scale-it"><a href="#">Find a consultant</a></div>
 		</div>
 	</div>
 </div>
